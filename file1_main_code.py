a0f59acf07f0de0f340c51bc70e471a66d46dd39
MNabeelAltaf <nabeelaltaf@outlook.com>
2023-05-30 00:14:01 +0500

    fake main code and test scripts are added

---
+++ file1_main_code.py
@@ -0,0 +1,2 @@
+print("practic for Jenkin")
+print("assume this is the mein code written in Python")
---
+++ file2_test_scripts.py
@@ -0,0 +1,2 @@
+print("practic for Jenkin")
+print("assume this is the test scripts of Main code i.e. file1")
