073e8d47990ff2ca44c050bd3e259e9bbeaa5533
MNabeelAltaf <89468956+MNabeelAltaf@users.noreply.github.com>
2023-06-19 23:44:53 +0500

    Update file2_test_scripts.py

--- file2_test_scripts.py
+++ file2_test_scripts.py
@@ -9,7 +9,6 @@ print("------------------------------")
 # from selinium.webdriver.common.keys import Keys
 
 # driver = webdriver.Chrome()
-
 # driver.get("http://www.example.com/login.html")
 
 # name_input =  driver.find_element_by_id("username")
@@ -28,5 +27,31 @@ print("------------------------------")
 # else:
 #   print("logn failed")
   
-driver.quit() 
+# driver.quit() 
+
+print("------------------------------")
+
+# print("this is test script of selinium python to validate all links in a web page")
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("https://www.example.com/index.html")
+
+links = driver.find_elements_by_tagname("a")
+
+for link in links:
+  href = link.get_attribute("href")
+  if href:
+    link.click()
+    
+    if statuscode==200:
+      print("valid link")
+    else:
+      print("invalid code")
+      
+    driver.back()
+    
+ driver.quit()
 
