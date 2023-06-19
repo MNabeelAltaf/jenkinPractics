bbeae181a8be734d0e9dc535f1d878501a81419a
MNabeelAltaf <89468956+MNabeelAltaf@users.noreply.github.com>
2023-06-19 23:31:46 +0500

    Update file2_test_scripts.py

--- file2_test_scripts.py
+++ file2_test_scripts.py
@@ -2,3 +2,31 @@ print("practic for Jenkin")
 print("assume this is the test scripts of Main code i.e. file1")
 print("adding github webhook...")
 print("------------------------------")
+
+# print("this is test script of selinium python to validate login form")
+
+# from selinium import webdriver
+# from selinium.webdriver.common.keys import Keys
+
+# driver = webdriver.Chrome()
+
+# driver.get("http://www.example.com/login.html")
+
+# name_input =  driver.find_element_by_id("username")
+# password_input =  driver.find_element_by_id("password")
+# loginBtn  = driver.find_element_by_id("loginBtn")
+
+# name_input.send_keys("anyUserName")
+# password_input.send_keys("anyPassword")
+
+# loginBtn.click()
+
+# welcome_msg = driver.find_element_by_id("welcome-msg").text
+
+# if "Welcome" in welcome_msg:
+#   print("sucessful login")
+# else:
+#   print("logn failed")
+  
+driver.quit() 
+
