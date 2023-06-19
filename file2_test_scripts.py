02b81e568eb16c1eb2b4165bd3285a413c054a1f
MNabeelAltaf <89468956+MNabeelAltaf@users.noreply.github.com>
2023-06-19 23:45:49 +0500

    Update file2_test_scripts.py

--- file2_test_scripts.py
+++ file2_test_scripts.py
@@ -33,25 +33,25 @@ print("------------------------------")
 
 # print("this is test script of selinium python to validate all links in a web page")
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("https://www.example.com/index.html")
+# driver = webdriver.Chrome()
+# driver.get("https://www.example.com/index.html")
 
-links = driver.find_elements_by_tagname("a")
+# links = driver.find_elements_by_tagname("a")
 
-for link in links:
-  href = link.get_attribute("href")
-  if href:
-    link.click()
+# for link in links:
+#   href = link.get_attribute("href")
+#   if href:
+#     link.click()
     
-    if statuscode==200:
-      print("valid link")
-    else:
-      print("invalid code")
+#     if statuscode==200:
+#       print("valid link")
+#     else:
+#       print("invalid code")
       
-    driver.back()
+#     driver.back()
     
- driver.quit()
+#  driver.quit()
 
